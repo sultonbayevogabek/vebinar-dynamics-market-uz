8302ee92d73574b6f6eb19b20ada777845577716
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-18 22:53:22 +0500

    initial commit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/+9oLYtsv0xvc4MDky';
+    window.location.href = 'https://t.me/zebodoctor_sbf1';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
